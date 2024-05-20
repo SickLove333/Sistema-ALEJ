b365be909a50ed9f1d70315e42096fcd9e94ce79
Anthony <anthonyarredondo.9mail.com>
2024-05-20 16:55:19 -0600

    pruebaaaaaaaaa

--- ALEJ/2 Espera.Designer.cs
+++ ALEJ/2 Espera.Designer.cs
@@ -52,18 +52,20 @@
             this.NavBar.Controls.Add(this.Inicio);
             this.NavBar.Controls.Add(this.panelLogo);
             this.NavBar.Location = new System.Drawing.Point(0, 0);
+            this.NavBar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.NavBar.Name = "NavBar";
-            this.NavBar.Size = new System.Drawing.Size(320, 1028);
+            this.NavBar.Size = new System.Drawing.Size(284, 822);
             this.NavBar.TabIndex = 1;
             // 
             // Registro
             // 
             this.Registro.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Registro.Font = new System.Drawing.Font("Montserrat", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Registro.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Registro.ForeColor = System.Drawing.Color.White;
-            this.Registro.Location = new System.Drawing.Point(0, 576);
+            this.Registro.Location = new System.Drawing.Point(0, 461);
+            this.Registro.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Registro.Name = "Registro";
-            this.Registro.Size = new System.Drawing.Size(320, 72);
+            this.Registro.Size = new System.Drawing.Size(284, 58);
             this.Registro.TabIndex = 5;
             this.Registro.Text = "REGISTRO";
             this.Registro.UseVisualStyleBackColor = true;
@@ -72,11 +74,12 @@
             // Archivo
             // 
             this.Archivo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Archivo.Font = new System.Drawing.Font("Montserrat", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Archivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Archivo.ForeColor = System.Drawing.Color.White;
-            this.Archivo.Location = new System.Drawing.Point(0, 504);
+            this.Archivo.Location = new System.Drawing.Point(0, 403);
+            this.Archivo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Archivo.Name = "Archivo";
-            this.Archivo.Size = new System.Drawing.Size(320, 72);
+            this.Archivo.Size = new System.Drawing.Size(284, 58);
             this.Archivo.TabIndex = 4;
             this.Archivo.Text = "ARCHIVO";
             this.Archivo.UseVisualStyleBackColor = true;
@@ -85,11 +88,12 @@
             // SubirReporte
             // 
             this.SubirReporte.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.SubirReporte.Font = new System.Drawing.Font("Montserrat", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SubirReporte.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.SubirReporte.ForeColor = System.Drawing.Color.White;
-            this.SubirReporte.Location = new System.Drawing.Point(0, 432);
+            this.SubirReporte.Location = new System.Drawing.Point(0, 346);
+            this.SubirReporte.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.SubirReporte.Name = "SubirReporte";
-            this.SubirReporte.Size = new System.Drawing.Size(320, 72);
+            this.SubirReporte.Size = new System.Drawing.Size(284, 58);
             this.SubirReporte.TabIndex = 3;
             this.SubirReporte.Text = "SUBIR REPORTE";
             this.SubirReporte.UseVisualStyleBackColor = true;
@@ -99,24 +103,26 @@
             // 
             this.REspera.BackColor = System.Drawing.Color.White;
             this.REspera.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.REspera.Font = new System.Drawing.Font("Montserrat", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.REspera.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.REspera.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(116)))), ((int)(((byte)(204)))));
-            this.REspera.Location = new System.Drawing.Point(0, 360);
+            this.REspera.Location = new System.Drawing.Point(0, 288);
+            this.REspera.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.REspera.Name = "REspera";
-            this.REspera.Size = new System.Drawing.Size(320, 72);
+            this.REspera.Size = new System.Drawing.Size(284, 58);
             this.REspera.TabIndex = 2;
-            this.REspera.Text = "REGISTRO DE ESPERA";
+            this.REspera.Text = "REGISTRO DE ESPERo";
             this.REspera.UseVisualStyleBackColor = false;
             this.REspera.Click += new System.EventHandler(this.REspera_Click);
             // 
             // Inicio
             // 
             this.Inicio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Inicio.Font = new System.Drawing.Font("Montserrat", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Inicio.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Inicio.ForeColor = System.Drawing.Color.White;
-            this.Inicio.Location = new System.Drawing.Point(0, 288);
+            this.Inicio.Location = new System.Drawing.Point(0, 230);
+            this.Inicio.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Inicio.Name = "Inicio";
-            this.Inicio.Size = new System.Drawing.Size(320, 72);
+            this.Inicio.Size = new System.Drawing.Size(284, 58);
             this.Inicio.TabIndex = 1;
             this.Inicio.Text = "INICIO";
             this.Inicio.UseVisualStyleBackColor = true;
@@ -126,27 +132,30 @@
             // 
             this.panelLogo.BackColor = System.Drawing.Color.White;
             this.panelLogo.Controls.Add(this.pictureBox1);
-            this.panelLogo.Location = new System.Drawing.Point(0, 72);
+            this.panelLogo.Location = new System.Drawing.Point(0, 58);
+            this.panelLogo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panelLogo.Name = "panelLogo";
-            this.panelLogo.Size = new System.Drawing.Size(320, 144);
+            this.panelLogo.Size = new System.Drawing.Size(284, 115);
             this.panelLogo.TabIndex = 0;
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(64, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(57, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(192, 138);
+            this.pictureBox1.Size = new System.Drawing.Size(171, 110);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1898, 1024);
+            this.ClientSize = new System.Drawing.Size(1687, 819);
             this.Controls.Add(this.NavBar);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form2";
             this.ShowIcon = false;
             this.Text = "ALEJ - Registro de Espera";
